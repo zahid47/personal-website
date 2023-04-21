88e4cbe44502cec467965d5b0d1696eddce4523f
zahid <epiczahid@gmail.com>
2023-04-21 15:58:39 +0600

    animated cta

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,15 @@ module.exports = {
       fontFamily: {
         pally: ["Pally-Variable", ...defaultTheme.fontFamily.sans],
       },
+      animation: {
+        background: "background ease infinite",
+      },
+      keyframes: {
+        background: {
+          "0%, 100%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+        },
+      },
     },
   },
   plugins: [],
