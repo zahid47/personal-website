e831c0252e99ac3bf0fa20a8bc4c990f283fc00c
zahid <epiczahid@gmail.com>
2023-04-22 20:15:11 +0600

    dynamic lang

---
+++ src/i18n/ui.ts
@@ -0,0 +1,15 @@
+export const languages = {
+  en: "English",
+  jp: "Japanese",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    title: "Portfolio of Zahid",
+  },
+  jp: {
+    title: "ザヒドのポートフォリオ",
+  },
+} as const;
