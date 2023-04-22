a446ec9793fc27cadd42e99c7b3ee3d9c150a47e
zahid <epiczahid@gmail.com>
2023-04-22 21:08:09 +0600

    minor

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -8,6 +8,7 @@ export function getLangFromUrl(url: URL) {
 
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
+    //@ts-ignore (jp object doesn't have some keys, in those cases, use the default language)
     return ui[lang][key] || ui[defaultLang][key];
   };
 }
