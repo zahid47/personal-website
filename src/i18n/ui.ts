514b565d2229376544dbc00916dee7700cd08467
zahid <epiczahid@gmail.com>
2023-04-22 21:03:31 +0600

    add jp ui

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,16 +8,92 @@ export const defaultLang = "en";
 export const ui = {
   en: {
     title: "Portfolio of Zahid",
+
     "hero.yahallo": "Yahallo!",
     "hero.nice-to-meet-you": "Nice to meet you",
-    "hero.intro":
-      "I am a software engineer based in Dhaka. I build web applications, discord bots, video games and everything in between.",
+    "hero.intro": "I am a software engineer based in Dhaka. I build web applications, discord bots, video games and everything in between.",
+
+    "journey.my-journey": "My Journey",
+    "journey.1": "2014",
+    "journey.1-text": "I started tinkering with computers when I was at high school and my fascination with technology began. Started learning coding and design.",
+    "journey.2-text": "Started working as a freelance UI/UX designer and contributed to multiple products that successfully launched in app stores.",
+    "journey.3-text": "Pursued my bachelor's degree in Software Engineering.",
+    "journey.4-text": "Got my first job as a software engineer. Helped launch a new startup by contributing to key features.",
+    "journey.5": "2022 - Current",
+    "journey.5-text": "Got admitted to the B-JET program where I'm learning Japanese language, culture and business manners.",
+    "journey.6": "Beyond",
+    "journey.6-text": "I want to work as a software engineer in Japan and contrubute to impactful projects that will make a difference in people's lives.",
+
+    "projects.projects": "Projects",
+
+    "projects.1.time": "Sept 2022",
+    "projects.1.title": "Quiz App",
+    "projects.1.description": "A sophisticated quiz platform, which allows users to generate and customize quizzes with an indefinite number of questions, each containing an infinite number of options.",
+
+    "projects.2.time": "April 2023",
+    "projects.2.title": "sharp-watermark",
+    "projects.2.description": "An open-source NPM package to easily add watermarks to your photos",
+    
+    "projects.3.time": "June 2022 - August 2022",
+    "projects.3.title": "Pizza App",
+    "projects.3.description": "A comprehensive pizza shop application, complete with features such as live order processing using socket.io.",
+    
+    "projects.4.time": "December 2021 - April 2022",
+    "projects.4.title": "Mixify",
+    "projects.4.description": "A customized playlist of remixed songs, derived from an existing Spotify playlist. The app enables users to enjoy their favorite tracks in a fresh and exciting way.",
+    
+    "projects.5.time": "February 2021 - March 2021",
+    "projects.5.title": "CR2",
+    "projects.5.description": "A script to extract and simplify the complex class schedules of our university, providing users with an easily digestible version that solely features their respective classes.",
+    
+    "projects.6.time": "September 2020 - July 2022",
+    "projects.6.title": "MuteAll",
+    "projects.6.description": "A Discord bot to mute all members within a voice channel. It has gained widespread popularity, being used in over 8k servers with nearly 1M members across diverse communities.",
+    
+    "footer.cat": "Cat Suit icon created by Denis Sazhin from Noun Project.",
+    "footer.copy": "Designed & Developed by Zahid",
   },
   jp: {
     title: "ザヒドのポートフォリオ",
+    
     "hero.yahallo": "ヤハロ！",
     "hero.nice-to-meet-you": "お会いできて嬉しいです。",
-    "hero.intro":
-      "私はダッカを拠点とするソフトウェア エンジニアです。Web アプリケーション、discord ボット、ビデオ ゲームなど、あらゆるものを構築しています。",
+    "hero.intro": "私はダッカを拠点とするソフトウェア エンジニアです。Web アプリケーション、discord ボット、ビデオ ゲームなど、あらゆるものを構築しています。",
+
+    "journey.my-journey": "私の旅",
+    "journey.1": "2014年",
+    "journey.1-text": "高校生のときにコンピューターをいじり始め、テクノロジーに魅了され始めました。コーディングとデザインを学び始めました。",
+    "journey.2-text": "フリーランスのUI / UXデザイナーとして働き始め、アプリストアでの立ち上げに成功した複数の製品に貢献しました。",
+    "journey.3-text": "ソフトウェア工学の学士号を取得しました。",
+    "journey.4-text": "ソフトウェアエンジニアとしての最初の仕事を得ました。主要な機能に貢献することで、新しいスタートアップの立ち上げを支援しました。",
+    "journey.5": "2022年 - 現在",
+    "journey.5-text": "日本語、日本文化、ビジネスマナーを学ぶB-JETプログラムに入学しました。",
+    "journey.6": "下",
+    "journey.6-text": "私は日本でソフトウェア エンジニアとして働き、人々の生活に変化をもたらす影響力のあるプロジェクトに貢献したいと思っています。",
+
+    "projects.projects": "プロジェクト",
+
+    "projects.1.time": "2022年9月",
+    "projects.1.title": "クイズアプリ",
+    "projects.1.description": "洗練されたクイズ プラットフォーム。ユーザーは、無数の質問を含むクイズを生成およびカスタマイズできます。それぞれの質問には無限の数のオプションが含まれています。",
+
+    "projects.2.time": "2023年4月",
+    "projects.2.description": "写真に透かしを簡単に追加できるオープンソースの NPM パッケージ",
+    
+    "projects.3.time": "2022年6月～2022年8月",
+    "projects.3.title": "ピザアプリ",
+    "projects.3.description": "socket.io を使用したライブ注文処理などの機能を備えた、包括的なピザ ショップ アプリケーションです。",
+    
+    "projects.4.time": "2021年12月～2022年4月",
+    "projects.4.description": "既存の Spotify プレイリストから派生した、リミックスされた曲のカスタマイズされたプレイリスト。このアプリにより、ユーザーはお気に入りの曲を新鮮でエキサイティングな方法で楽しむことができます。",
+    
+    "projects.5.time": "2021年2月～2021年3月",
+    "projects.5.description": "私たちの大学の複雑な授業スケジュールを抽出して簡素化するスクリプトで、それぞれの授業だけを取り上げた簡単に消化できるバージョンをユーザーに提供します。",
+    
+    "projects.6.time": "2020年9月～2022年7月",
+    "projects.6.description": "音声チャネル内のすべてのメンバーをミュートする Discord ボット。 幅広い人気を博し、8,000 を超えるサーバーで使用され、さまざまなコミュニティに 100 万人近くのメンバーがいます。",
+
+    "footer.cat": "Noun Project の Denis Sazhin によって作成されたキャット スーツのアイコン。",
+    "footer.copy": "Zahidによって設計および開発されました",
   },
 } as const;
