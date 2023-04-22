559203752c106a762b9b37224ac92adace48032e
zahid <epiczahid@gmail.com>
2023-04-22 20:22:01 +0600

    hero lang

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,8 +8,16 @@ export const defaultLang = "en";
 export const ui = {
   en: {
     title: "Portfolio of Zahid",
+    "hero.yahallo": "Yahallo!",
+    "hero.nice-to-meet-you": "Nice to meet you",
+    "hero.intro":
+      "I am a software engineer based in Dhaka. I build web applications, discord bots, video games and everything in between.",
   },
   jp: {
     title: "ザヒドのポートフォリオ",
+    "hero.yahallo": "ヤハロ！",
+    "hero.nice-to-meet-you": "お会いできて嬉しいです。",
+    "hero.intro":
+      "私はダッカを拠点とするソフトウェア エンジニアです。Web アプリケーション、discord ボット、ビデオ ゲームなど、あらゆるものを構築しています。",
   },
 } as const;
